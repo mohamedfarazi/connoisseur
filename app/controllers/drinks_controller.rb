911b1702b017c9781fcf97e5b2b60a9bbb9dd8a4
Mohamed Farazi <mohamed.farazi@gmail.com>
2014-02-06 21:44:43 -0500

    Got stouts page working, going to move on to the rest

--- app/controllers/drinks_controller.rb
+++ app/controllers/drinks_controller.rb
@@ -2,8 +2,18 @@ require 'open-uri'
 
 class DrinksController < ApplicationController
   def index
-    drinks_json = open('http://lcboapi.com/products').read
+    @time = Time.now
+    @t = (@time.hour)
+    page = params[:page] || 1
+
+    drinks_json = open("http://lcboapi.com/products?page=#{page}").read
     @drinks = JSON.parse(drinks_json)
+
+    stouts_json = open("http://lcboapi.com/products?q=stout").read
+    @stouts = JSON.parse(stouts_json)
+
+    porter_json = open("http://lcboapi.com/products?q=porter").read
+    @porters = JSON.parse(porter_json)
   end
 
   def new
@@ -22,7 +32,13 @@ class DrinksController < ApplicationController
   end
 
   def show
+    id = params[:id]
+    product_json = open("http://lcboapi.com/products/#{id}").read
+    @product = JSON.parse(product_json)
   end
 
-
+  def stouts
+    stouts_json = open("http://lcboapi.com/products?q=stout").read
+    @stouts = JSON.parse(stouts_json)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Connoisseur::Application.routes.draw do
   resources :drinks
-  root 'drinks#index'
+  get 'drinks_stouts' => "drinks#stouts"
+  root to: 'drinks#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
